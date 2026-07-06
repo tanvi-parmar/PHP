3bdb3f367475d92695454021ea9139da0e90904c
tanvi-parmar <tanviparmar357@gmail.com>
2026-07-06 11:22:38 +0530

    Add files via upload

---
+++ ass-1 q-3.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+	<title>ass-1 q-3</title>
+</head>
+<body>
+
+<?php	
+	$a=10;
+	$b=20; 
+?>
+<table align="center" border="1" height="30%" width="40%">
+
+	<tr>
+	<td><b>Operations</b></td>
+	<td><b>Answers</b></td>
+	</tr>
+	
+	<tr>
+	<td><b>Value of A:</b></td>
+	<td><?php echo $a ?></td>
+	</tr>
+	
+	<tr>
+	<td><b>Value of B:</b></td>
+	<td><?php echo $b ?></td>
+	</tr>
+	
+	<tr>
+	<td><b>Addition of A and B:</b></td>
+	<td><?php echo ($a+$b) ?></td>
+	</tr>
+	
+	<tr>
+	<td><b>Substraction of A and B:</b></td>
+	<td><?php echo ($a-$b) ?></td>
+	</tr>
+	
+	<tr>
+	<td><b>Multiplication of A and B:</b></td>
+	<td><?php echo ($a*$b) ?></td>
+	</tr>
+	
+	<tr>
+	<td><b>Division of A and B:</b></td>
+	<td><?php echo ($a/$b) ?></td>
+	</tr>
+</table>
+</body>
+</html>
